c1c4c4a6004eb17adaf1a7d3481dc0d635972885
Jeff Wiedemann <jwiedemann@splunk.com>
2018-01-10 10:00:03 -0600

    Should have checked in with load test refactor. These are helper commands that make authoring load tests a little easier

---
+++ helpers/siteInteractionHelpers.py
@@ -0,0 +1,29 @@
+import base64
+
+def viewCatalog(taskSet):
+	taskSet.client.get("/category.html", headers={"User-Agent":taskSet.uaString})
+
+def viewItemDetails(taskSet, item_id):
+	taskSet.client.get("/detail.html?id={}".format(item_id), headers={"User-Agent":taskSet.uaString})
+
+def applyFilter(taskSet):
+	taskSet.client.get("/category.html?tags=brown", headers={"User-Agent":taskSet.uaString})
+
+def login(taskSet, username, password):
+	base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
+	taskSet.client.get("/login", headers={"Authorization":"Basic {0}".format(base64string), "User-Agent":taskSet.uaString})
+
+def clearCart(taskSet):
+	taskSet.client.delete("/cart", headers={"User-Agent":taskSet.uaString})
+
+def addItemToCart(taskSet, item_id, quantity):
+	taskSet.client.post("/cart", json={"id": item_id, "quantity": quantity}, headers={"User-Agent":taskSet.uaString})
+
+def viewCart(taskSet):
+	taskSet.client.get("/basket.html", headers={"User-Agent":taskSet.uaString})
+
+def purchaseCart(taskSet):
+	taskSet.client.post("/orders", headers={"User-Agent":taskSet.uaString})
+
+def viewHomepage(taskSet):
+	taskSet.client.get("/", headers={"User-Agent":taskSet.uaString})
